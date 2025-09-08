7f57136419a8ab31138315451e197d766e2c41cf
study460 <regularstudy460@gmail.com>
2025-09-08 22:29:25 +0600

    some edit

--- index.js
+++ index.js
@@ -50,3 +50,78 @@ const displayMenu = (categories) => {
         container.appendChild(li);
     });
 }
+
+
+
+
+// Cards
+const loadTreeCard = async (id) => {
+    spinner.classList.remove("hidden");
+    const res = await fetch(`https://openapi.programming-hero.com/api/category/${id}`);
+    const data = await res.json();
+    displayLoadTreeCard(data.plants);
+    spinner.classList.add("hidden");
+}
+
+const allCategory = async () => {
+    spinner.classList.remove("hidden");
+    const res = await fetch(`https://openapi.programming-hero.com/api/plants`);
+    const data = await res.json();
+    displayLoadTreeCard(data.plants);
+    spinner.classList.add("hidden");
+}
+
+const displayLoadTreeCard = (trees) => {
+    const container = document.getElementById("card-cointainer");
+    container.innerHTML = "";
+    trees.forEach(tree => {
+        const div = document.createElement("div");
+        div.classList.add("tree-card", "bg-white", "p-4", "rounded-xl", "shadow-md");
+        div.innerHTML = `
+      <img src="${tree.image}" class="plant-img mb-2">
+      <h1 onclick="loadNameDetails(${tree.id})" class="font-bold text-xl cursor-pointer tree-name">${tree.name}</h1>
+      <p class="text-sm mb-2">${tree.description}</p>
+      <div class="flex justify-between mb-2">
+        <span class="bg-green-100 px-2 py-1 rounded text-green-800">${tree.category}</span>
+        <span class="font-bold">৳<span class="tree-price">${tree.price}</span></span>
+      </div>
+      <button class="w-full bg-green-800 text-white py-2 rounded add-cart hover:bg-green-700">Add to Cart</button>
+    `;
+        container.appendChild(div);
+    });
+}
+
+// Add to Cart
+document.getElementById("card-cointainer").addEventListener("click", (e) => {
+    if (e.target.classList.contains("add-cart")) {
+        const card = e.target.closest(".tree-card");
+        const name = card.querySelector(".tree-name").innerText;
+        const price = parseFloat(card.querySelector(".tree-price").innerText);
+        addToCart(name, price);
+    }
+});
+
+function addToCart(name, price) {
+    const cart = document.getElementById("add-to-cart-main-section");
+
+    const cartItem = document.createElement("div");
+    cartItem.className = "cart-item flex justify-between items-center rounded-lg p-2 bg-green-50 mt-2";
+    cartItem.innerHTML = `
+      <div>
+        <h1 class="font-bold">${name}</h1>
+        <p>৳<span class="cart-item-price">${price}</span></p>
+      </div>
+      <button class="remove-btn">❌</button>`;
+
+    cart.appendChild(cartItem);
+    updateCartTotal();
+}
+
+
+// Remove from cart
+document.getElementById("add-to-cart-main-section").addEventListener("click", (e) => {
+    if (e.target.classList.contains("remove-btn")) {
+        e.target.closest(".cart-item").remove();
+        updateCartTotal();
+    }
+});
